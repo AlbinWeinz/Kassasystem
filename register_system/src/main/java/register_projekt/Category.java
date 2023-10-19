f6411f8a0674afb67a3c373786dfd75fec520bbe
itisschasse <147489388+itisschasse@users.noreply.github.com>
2023-10-20 01:27:08 +0200

    Add files via upload

---
+++ register_system/src/main/java/register_projekt/Category.java
@@ -0,0 +1,107 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+public class Category {
+    Map<String, Double> categoryMap= new HashMap<>();
+
+    public Category(){
+        categoryMap=new HashMap<>();
+    }
+
+    public int getProductCount() {
+        return categoryMap.size();
+    }
+    public Double getProductPrice(String productName) {
+        return categoryMap.get(productName);
+    }
+    public boolean addToCategory(Product product) {
+        String productName = product.getProductName();
+        if (categoryMap.containsKey(productName)) {
+            return false;
+        } else {
+            categoryMap.put(productName, product.getProductPrice());
+            return true;
+        }
+    }
+
+    public boolean containsProduct(String productName) {
+        return categoryMap.containsKey(productName);
+    }
+
+    public void updateProductPrice(String productName, Double newPrice) {
+        if (categoryMap.containsKey(productName)) {
+            categoryMap.put(productName, newPrice);
+            System.out.println("Price of " + productName + " updated to " + newPrice);
+        } else {
+            System.out.println("Product not found: " + productName);
+        }
+    }
+    public void removeProductFromCategory(Product product) {
+        if (categoryMap.isEmpty()) {
+            throw new IllegalStateException("Category is empty. Cannot remove from an empty category.");
+        }
+        categoryMap.remove(product.getProductName());
+    }
+    public boolean validProductPrice(Product product) {
+        if (product.getProductPrice() < 0) {
+            System.out.println("Invalid product price: Price cannot be negative.");
+            return false;
+        }
+
+        if (categoryMap.containsKey(product.getProductName())) {
+            System.out.println("Product with the same name already exists in the category.");
+            return false;
+        }
+
+        return true;
+    }
+
+    public void clearCategory() {
+        categoryMap.clear();
+    }
+    public List<Product> sortProductsByName() {
+        if (categoryMap.isEmpty()) {
+            throw new IllegalStateException("Cannot sort an empty category.");
+        }
+        List<Product> sortedProducts = new ArrayList<>();
+        List<String> productNames = new ArrayList<>(categoryMap.keySet());
+        Collections.sort(productNames);
+
+        for (String productName : productNames) {
+            Double productPrice = categoryMap.get(productName);
+            Product product=new Product(productName, productPrice);
+            sortedProducts.add(product);
+        }
+
+        return sortedProducts;
+    }
+
+    public List<Product> sortProductsByPrice() {
+        if (categoryMap.isEmpty()) {
+            throw new IllegalStateException("Cannot sort an empty category.");
+        }
+        List<Product> sortedProducts = new ArrayList<>();
+        List<Map.Entry<String, Double>> productEntries = new ArrayList<>(categoryMap.entrySet());
+        Collections.sort(productEntries, Comparator.comparing(Map.Entry::getValue));
+
+        for (Map.Entry<String, Double> entry : productEntries) {
+            String productName = entry.getKey();
+            Double productPrice = entry.getValue();
+            sortedProducts.add(new Product(productName, productPrice));
+        }
+
+        return sortedProducts;
+    }
+    public void categoryError(Exception e) {
+        System.err.println("An error occurred: " + e.getMessage());
+        e.printStackTrace();
+    }
+
+
+}
+
+
+
---
+++ register_system/src/main/java/register_projekt/Product.java
@@ -0,0 +1,46 @@
+import java.util.Objects;
+
+public class Product {
+
+    public String productName;
+    public Double productPrice;
+
+    public Product(String productName, Double productPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+
+
+    }
+
+    public String getProductName() {
+        return this.productName;
+
+    }
+
+    public Double getProductPrice() {
+        return this.productPrice;
+    }
+
+
+    @Override
+    public String toString() {
+        return "" + productName + "Price: " + productPrice;
+    }
+
+    @Override
+    public boolean equals(Object product) {
+        if (this == product) {
+            return true;
+        }
+        if (product == null || getClass() != product.getClass()) {
+            return false;
+        }
+        Product otherProduct = (Product) product;
+        return Objects.equals(productName, otherProduct.productName) && Objects.equals(productPrice, otherProduct.productPrice);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productName, productPrice);
+    }
+}
